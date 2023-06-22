c8872ee44370a9997d162667321e91ef5da37fdf
Edgar Diaz <ed35376@gmail.com>
2023-06-21 22:19:00 -0400

    first commit

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planet, Favorites
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,141 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/users', methods=['GET'])
+def list_users():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    users = User.query.all()
+    users = list(map (lambda item: item.serialize(), users ))
 
-    return jsonify(response_body), 200
+    return jsonify(users), 200
+    
+@app.route('/people', methods=['GET'])
+def list_people(): 
+    if request.method == 'GET':
+        person=People()
+        person=person.query.all()
+        person=list(map (lambda item: item.serialize(), person ))
+        return jsonify(person) ,200
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_people(people_id):
+      if request.method == 'GET':
+        persona=People()
+        persona = persona.query.get(people_id)
+        persona=persona.serialize()
+        return jsonify(persona), 200
+
+
+@app.route('/planets', methods=['GET'])
+def planet():
+    if request.method == 'GET':
+        world=Planet()
+        world=world.query.all()
+        world=list(map (lambda item: item.serialize(), world ))
+        return jsonify(world) ,200
+
+@app.route('/planet/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    if request.method == 'GET':
+        planet=Planet()
+        planet = planet.query.get(planet_id)
+        planet=planet.serialize()
+        return jsonify(planet), 200
+
+@app.route('/users/favorites/<int:user_id>', methods=['GET'])
+def list_user_favorites(user_id):
+    
+    favorites = Favorites.query.filter_by(user_id=user_id).all()
+    favorites=list(map (lambda item: item.serialize(), favorites ))
+    return jsonify(favorites), 200
+
+@app.route('/favorite/planet/<int:planet_id>/', methods=['POST'])
+def add_planet_favorite(planet_id):
+    if request.method =="POST":
+        data = request.json
+
+        planet = Planet.query.get(planet_id)
+        if planet is None:
+            return jsonify({"message": "Planet not found "}), 404
+
+        existing_favorite = Favorites.query.filter_by(user_id=data["user_id"], planet_id=planet_id).first()
+        if existing_favorite:
+            return jsonify({"message": "Favorite already exists "}), 404
+
+        new_favorite = Favorites(user_id=data["user_id"], planet_id=planet_id)
+        
+        db.session.add(new_favorite)
+        try:
+            db.session.commit()
+            return jsonify({"message": "Planet added "}), 200
+        except Exception as error:
+            return jsonify({"error":error.args}) , 500
+        
+        
+@app.route('/favorite/people/<int:people_id>', methods=['POST'])
+def add_people_favorite(people_id):
+    if request.method =="POST":
+        data = request.json
+
+        people = People.query.get(people_id)
+        if people is None:
+            return jsonify({"message": "Character not found "}), 404
+
+        existing_favorite = Favorites.query.filter_by(user_id=data["user_id"], people_id=people_id).first()
+        if existing_favorite:
+            return jsonify({"message": "Favorite already exists "}), 404
+
+        new_favorite = Favorites(user_id=data["user_id"], people_id=people_id)
+        db.session.add(new_favorite)
+        try:
+            db.session.commit()
+            return jsonify({"message": "Character added "}), 200
+        except Exception as error:
+            return jsonify({"error":error.args}) , 500
+
+@app.route('/favorite/planet/<int:planet_id>', methods = ["DELETE"])
+def delete_planet(planet_id):
+    if request.method == "DELETE":
+        data = request.json
+
+        planet = Planet.query.get(planet_id)
+        if planet is None:
+            return jsonify({"message": "Planet not found "}), 404
+
+        favorite = Favorites.query.filter_by(user_id=data["user_id"], planet_id=planet_id).first()
+        if favorite is None:
+            return jsonify({"message": "Favorite not found "}), 404
+
+        db.session.delete(favorite)
+        try:
+            db.session.commit()
+            return jsonify({"message": "Planet removed "}), 200
+        except Exception as error:
+            return jsonify({"error": error.args}), 500
+    
+
+
+
+@app.route('/favorite/people/<int:people_id>', methods = ["DELETE"])
+def delete_people(people_id):
+     if request.method == "DELETE":
+        data = request.json
+
+        people = People.query.get(people_id)
+        if people is None:
+            return jsonify({"message": "People not found "}), 404
+
+        favorite = Favorites.query.filter_by(user_id=data["user_id"], people_id=people_id).first()
+        if favorite is None:
+            return jsonify({"message": "Favorite not found "}), 404
+
+        db.session.delete(favorite)
+        try:
+            db.session.commit()
+            return jsonify({"message": "People removed "}), 200
+        except Exception as error:
+            return jsonify({"error": error.args}), 500
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -5,15 +5,68 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean, unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.name
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "name": self.name
             # do not serialize the password, its a security breach
         }
+
+class People(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(100), nullable=False)
+    height=db.Column(db.String(100), nullable=False)
+    mass=db.Column(db.String(100), nullable=False)
+    birth_year=db.Column(db.String(100), nullable=False)
+    eye_color=db.Column(db.String(100), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "heigth": self.height,
+            "mass": self.mass,
+            "birth_year": self.birth_year,
+            "eye_color": self.eye_color           
+        }
+
+class Planet(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(100), nullable=False)
+    rotation_period=db.Column(db.String(100), nullable=False)
+    orbital_period=db.Column(db.String(100), nullable=False)
+    diameter=db.Column(db.String(100), nullable=False)
+    climate=db.Column(db.String(100), nullable=False)
+    
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "diameter": self.diameter,
+            "climate": self.climate            
+        }
+    
+class Favorites(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    planet_id=db.Column(db.Integer,db.ForeignKey('planet.id'))
+    people_id=db.Column(db.Integer, db.ForeignKey('people.id'))
+    user_id=db.Column(db.Integer, db.ForeignKey('user.id') )
+
+    def serialize(self):
+        return {
+            "id":self.id,
+            "planet_id": self.planet_id,
+            "people_id": self.people_id,
+            "user_id": self.user_id
+        }
+    
